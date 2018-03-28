ac8c55c8c91fa2081ba25f2766e532dcd53e11fe
RenBrian <Brian_Ren@outlook.com>
2018-03-28 17:44:24 +0800

    "js coding"

--- oldneighborhood-site-management/src/main/resources/static/js/sitelist.js
+++ oldneighborhood-site-management/src/main/resources/static/js/sitelist.js
@@ -32,6 +32,42 @@ $().ready(function() {
 		datatype: "json"
 	})
 	
+	$("#newsite").click(function(){
+		var sitebody = $("#sitebody");
+		addsiteHTML = '<div class="card mb-3"><div class="card-header"><i class="fa fa-pencil"></i> 新增景点</div>'
+			+ '<div class="card-body"><div class="table-responsive"><form role="form">'
+				+ '<div class="form-group"><label for="site_name">名称：</label> <input type="text" class="form-control w-50" id="site_name" placeholder="北大楼" /></div>'
+				+ '<div class="form-group"><label for="site_address">地址：</label> <input type="text" class="form-control w-50" id="site_address" placeholder="汉口路22号" /></div>'
+				+ '<div class="form-group"><label for="site_type">类型（景区/停车场/卫生间）：</label> <input type="text" class="form-control w-50" id="site_type" /></div>'
+				+ '<div class="form-group"><label for="site_ticket">门票费用(元)：</label> <input type="text" class="form-control w-50" id="site_ticket" placeholder="20.00" /></div>'
+				+ '<div class="form-group"><label for="site_time">开放时间：</label> <input type="text" class="form-control w-50" id="site_time" placeholder="周一~周五：10:00-17:00\n周末：10:00-18:00" /></div>'
+				+ '<button id="release" type="button" class="btn btn-default">发布</button></form></div></div></div>';
+		sitebody.html(addsiteHTML);
+		
+		$("#release").click(function(){
+			$.ajax({
+	            url: 'http://localhost:8082/oldneighborhood/site/add',
+	            type: 'post',
+	            contentType:'application/json',
+	            data: JSON.stringify({
+	            	"site_name":$("#site_name"),
+	            	"site_address":$("#site_address"),
+	            	"site_type":$("#site_type"),
+	            	"site_ticket":$("#site_ticket"),
+	            	"site_time":$("#site_time")
+	            	}),
+//	            contentType: false,   // 告诉jQuery不要去设置Content-Type请求头
+	            success: function (data) {   //成功后回调
+	            	toastr.success("新增成功！");
+	                console.log(data);
+	            },
+	            error: function(e){    //失败后回调
+	                toastr.error("出错了");
+	            },
+	            datatype: "json"
+	    	})
+		})
+	})
 	
 });
 
