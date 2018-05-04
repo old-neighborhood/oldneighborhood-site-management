de331cb4e482c989bdec281f47b5643b19d10adc
RenBrian <Brian_Ren@outlook.com>
2018-05-04 14:33:56 +0800

    "5/4"

--- oldneighborhood-site-management/src/main/java/com/oldneighborhood/demo/OldneighborhoodSiteManagementApplication.java
+++ oldneighborhood-site-management/src/main/java/com/oldneighborhood/demo/OldneighborhoodSiteManagementApplication.java
@@ -2,14 +2,14 @@ package com.oldneighborhood.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
+//import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
-@ComponentScan("com.oldneighborhood.demo")
+//@EnableEurekaClient
+//@EnableFeignClients
+//@ComponentScan("com.oldneighborhood.demo")
 public class OldneighborhoodSiteManagementApplication {
 
 	public static void main(String[] args) {
